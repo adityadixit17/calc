f1c121ac7c9adba7aed2924d09acaa3c3eacc9a8
aditya dixit <adirkt2003@gmail.com>
2025-01-18 12:12:16 +0530

    this is for division

---
+++ src/division.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class division {
+    public static void main(String[] args) {
+        Scanner sx=new Scanner(System.in);
+        int a= sx.nextInt();
+        int b= sx.nextInt();
+        System.out.println(a/b);
+    }
+}
