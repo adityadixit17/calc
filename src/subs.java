198b7b10ed4bb6fbcb65297303e2bc669c115582
aditya dixit <adirkt2003@gmail.com>
2025-01-18 11:56:01 +0530

    this file is for substraction

---
+++ src/subs.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class subs {
+    public static void main(String[] args) {
+        Scanner sx=new Scanner(System.in);
+        int a= sx.nextInt();
+        int b= sx.nextInt();
+        int c= sx.nextInt();
+        System.out.println(a-b-c);
+    }
+}
